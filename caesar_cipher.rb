efe0a92bbbbd146530071f379ac11775880ccbae
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-03 23:19:15 +0500

    Core logic of encryption implement
    all core logic need to create encryption is complete wrapping from z to a left

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,15 @@
-# first We need to take user input
+# Taking user input message to encrypt
+puts "Enter the message"
 message = gets.chomp
+# Getting key from the user
+puts "Enter the Shift"
+shift = gets.chomp.to_i
 
-puts message
+# take the message and turn it in to array of character
+arry = message.chars.map{|char| char.ord}
+shifted_message = arry.map {|value| value + shift}
+encryption = shifted_message.map{|value| value.chr}
+final_message = encryption.join
+# before i put the below line in action i first need to solve the wraping problem
+# result = final_message.gsub('!',' ')
+# p result

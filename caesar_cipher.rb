9c6a4ff791f973a321526bdf966b6b43454e53e9
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-04 21:14:47 +0500

    Encrytpion achive for uppercase letter
    encrytpion for the uppercase letter is achive handling lowercase remains

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,11 +5,27 @@ message = gets.chomp
 puts "Enter the Shift"
 shift = gets.chomp.to_i
 
-# take the message and turn it in to array of character
+# take the message and turn it in to array of character and assigning ascii
 arry = message.chars.map{|char| char.ord}
-shifted_message = arry.map {|value| value + shift}
+
+# adding shift to the message while avoiding the spaces
+shifted_message = arry.map do |value|
+  if value.ord != 32
+    shifted_value = value+shift
+    if shifted_value > 90
+      shifted_value -= 26
+    end
+  else
+    value
+  end
+end
+
+
+# bringing the shifted ascii value back to the form of character
 encryption = shifted_message.map{|value| value.chr}
+
+# turning that array of shifted character making it string
 final_message = encryption.join
-# before i put the below line in action i first need to solve the wraping problem
-# result = final_message.gsub('!',' ')
-# p result
+
+# print the final encrypted
+puts final_message
